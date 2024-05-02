b2f2e89ee2bdd1d6c3d40e5eb7f533c2facbe7eb
ysgiraldo <ygiraldoca@unal.edu.co>
2024-05-01 21:51:42 -0500

    medial users

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 # This form will be used to authenticate users against the database. Note that you use the PasswordInput
 # widget to render the password HTML element. This will include type="password" in the HTML so
@@ -24,3 +25,13 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
